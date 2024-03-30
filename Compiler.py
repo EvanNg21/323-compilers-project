f7e4a523c4f764b1c982b0a6d41de635fb151d86
Evan <21ewng@gmail.com>
2024-03-29 22:03:05 -0700

    readfile

---
+++ Compiler.py
@@ -0,0 +1,16 @@
+import re
+
+#read the file
+def read_file(file):
+    lines = []
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    return lines
+def main():
+    # Define the file
+    file = "input.txt"
+    # Read the lines from the file
+
+
+if __name__ == "__main__":
+    main()
