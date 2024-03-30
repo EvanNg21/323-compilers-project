b3897270ad2a99084ce707b44b2c428ffa02a6d4
Evan <21ewng@gmail.com>
2024-03-29 22:09:08 -0700

    rmove spaces

--- Compiler.py
+++ Compiler.py
@@ -1,11 +1,21 @@
 import re
 
-#read the file
+#read the
 def read_file(file):
     lines = []
     with open(file, 'r') as f:
         lines = f.readlines()
     return lines
+#remove the spaces from each line
+def remove_spaces(lines):
+    no_spaces = []
+    for line in lines:
+        # Remove comments
+        line = re.sub(r'#.*', '', line)
+        # Remove spaces
+        line_without_spaces = line.replace(" ", "")
+        no_spaces.append(line_without_spaces)
+    return no_spaces
 def main():
     # Define the file
     file = "input.txt"
